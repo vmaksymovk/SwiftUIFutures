7010b1b55e156bc7eade3dc67259f958906103ad
Vladyslav Maksymov <vmaksymovk@gmail.com>
2024-05-28 00:15:20 +0200

    ScrollViewReader advanced search

---
+++ LearningSwiftUI/Views/ScrollViewReaderAdvanced2.swift
@@ -0,0 +1,51 @@
+
+import SwiftUI
+
+struct ScrollViewReaderAdvanced2: View {
+    @State var inputValue : Int? = nil
+    var body: some View {
+        VStack{
+            ScrollViewReader{ proxy in
+                ScrollView{
+                    LazyVStack(spacing: 15){
+                        ForEach(1..<300, id: \.self){ index in
+                            Text("Item #\(index)")
+                                .id(index)
+                                .foregroundStyle(.white)
+                                .font(.largeTitle)
+                                .padding()
+                                .background(
+                                    RoundedRectangle(cornerRadius: 40)
+                                        .fill(.green)
+                                )
+                            
+                        }
+                    }
+                }
+                
+                HStack{
+                    TextField("Scroll to", value: $inputValue, format: .number)
+                        .textFieldStyle(.roundedBorder)
+                        .padding(.leading, 10)
+                    Button (action: {
+                        if let index = inputValue, (1..<300).contains(index){
+                            withAnimation{
+                                proxy.scrollTo(inputValue, anchor: .center)
+                            }
+                        }
+                    })
+                    {
+                        Label("Retrieve", systemImage: "paperplane")
+                            .labelStyle(.iconOnly)
+                            .padding(.trailing, 10)
+                    }
+                    
+                }
+            }
+        }
+    }
+}
+
+#Preview {
+    ScrollViewReaderAdvanced2()
+}
