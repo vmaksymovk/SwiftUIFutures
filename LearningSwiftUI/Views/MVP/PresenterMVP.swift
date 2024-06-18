47542b1bb43f53367d294b8dd54416e59d179a67
Vladyslav Maksymov <vmaksymovk@gmail.com>
2024-06-18 15:17:55 +0200

    Added files + MVP group

--- LearningSwiftUI/Views/MVP/PresenterMVP.swift
+++ LearningSwiftUI/Views/MVP/PresenterMVP.swift
@@ -1,7 +1,7 @@
 import Combine
 
 class CounterPresenter : ObservableObject {
-    @Published private(set) var count : Int = 0
+    @Published private(set) var count : Int = 0 // Declarate @Published only within class to mention who will Observable 
     private var model: CounterModel
     
     init(count: Int, model: CounterModel) {
@@ -11,11 +11,15 @@ class CounterPresenter : ObservableObject {
     
     func increment() {
         model.count += 1 // model updating
+        print("Model Updated!")
         count = model.count // Updated model to Presenter
+        print("Value of Presenter Updated and equal = \(count) !")
     }
     
     func decrement() {
         model.count -= 1
+        print("Model Updated!")
         count = model.count
+        print("Value of Presenter Updated and equal = \(count) !")
     }
 }
